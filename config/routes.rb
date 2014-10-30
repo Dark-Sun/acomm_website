54a8b811b30df20e98c1c7a77e16d01199fb5662
Andriy Lukashchuk <darksundarksun@gmail.com>
2014-10-30 19:14:30 +0200

    changes

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,10 @@ Rails.application.routes.draw do
 
   root to: 'pages#home'
 
-  get '/for_vendors' => 'pages#for_vendors', as: 'for_vendors'
-  get '/pricing'     => 'pages#pricing',     as: 'pricing'
-  get '/contact_us'  => 'pages#contact_us',  as: 'contact_us'
-
+  get '/for_vendors'      => 'pages#for_vendors',      as: 'for_vendors'
+  get '/pricing'          => 'pages#pricing',          as: 'pricing'
+  get '/contact_us'       => 'pages#contact_us',       as: 'contact_us'
+  get '/existing_vendors' => 'pages#existing_vendors', as: 'existing_vendors'
 
   get '/under_construction' => 'pages#under_construction', as: 'under_construction'
 end
